1e645f613c7a03e4dc1912c93f448ea96faf0141
asi1024 <akifumiimanishi@yahoo.co.jp>
2015-10-14 02:45:31 +0900

    fix

--- src/solver.cpp
+++ src/solver.cpp
@@ -26,7 +26,9 @@ Data solve(Stage st) {
     que.pop();
     if (s.score > dist[s.hash()]) continue;
     Stage ns = s;
-    if (++cnt > 1000000) return Data(Records(), cnt);
+    ++cnt;
+    if (cnt % 500000 == 0) std::cerr << cnt << " steps." << std::endl;
+    if (cnt > 5000000) return Data(Records(), cnt);
     for (int i = 1; i < 9; ++i) {
       for (int j = 1; j < 12; ++j) {
         if (ns.swap(i, j)) {
